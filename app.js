f0e9f8e7ace73f369f7eac77fd720d0b76d9890d
Swapnil <swapnilkha@gmail.com>
2020-03-07 14:11:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let userRoutes = require('./routes/user');
 
 const app = express();
 
-let dbUrl = "mongodb+srv://smyle:smylepass@cluster0-5zyat.mongodb.net/test?retryWrites=true&w=majority";;
+let dbUrl = "mongodb+srv://smyle:smylepass@cluster0-5zyat.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(dbUrl);
 
 require('./config/passport');
